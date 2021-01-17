206a8005508360b74f62b9a980007fec9abafde9
Joe May <jmmay1974@gmail.com>
2021-01-16 23:08:32 -0700

    updated all

---
+++ app.js
@@ -0,0 +1,33 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+	this.classList.toggle('flip');
+
+	if (!hasFlippedCard) {
+		//first click
+		hasFlippedCard = true;
+		firstCard = this;
+	} else {
+		//second click
+		hasFlippedCard = false;
+		secondCard = this;
+
+		//do cards match?
+		if (firstCard.dataset.img === secondCard.dataset.img) {
+			//match
+			firstCard.removeEventListener('click', flipCard);
+			secondCard.removeEventListener('click', flipCard);
+		} else {
+			//no match
+			setTimeout(() => {
+				firstCard.classList.remove('flip');
+				secondCard.classList.remove('flip');
+			}, 1200);
+		}
+	}
+}
+
+cards.forEach((card) => card.addEventListener('click', flipCard));
